9c5e62b5bbf458ad5699c7efeede966fd20b22c5
Daniel <github@profoundideation.com>
2018-05-31 23:30:54 -0400

    Multiples of 3 and 5
    Multiples of 3 and 5

--- 9-Coding Interview Questions And Take Home Assignments/6-Project Euler Problems/Problem 1-Multiples of 3 and 5.js
+++ 9-Coding Interview Questions And Take Home Assignments/6-Project Euler Problems/Problem 1-Multiples of 3 and 5.js
@@ -3,7 +3,7 @@
 function multiplesOf3and5(number) {  
   let sum = 0;
   
-  for (let i = 0; i <= number; i++) {
+  for (let i = 0; i < number; i++) {
     if (i % 3 === 0 || i % 5 === 0) {
       sum += i;
     }
