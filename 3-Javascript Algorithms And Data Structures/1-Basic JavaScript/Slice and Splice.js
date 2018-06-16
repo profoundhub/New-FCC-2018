c8611c3e94a631e50bdef78a7e50eea5adf8c5aa
Daniel <github@profoundideation.com>
2018-06-16 12:32:53 -0400

    Create Slice and Splice.js

---
+++ 3-Javascript Algorithms And Data Structures/1-Basic JavaScript/Slice and Splice.js
@@ -0,0 +1,9 @@
+function frankenSplice(arr1, arr2, n) {
+  // It's alive. It's alive!
+
+  let arr0 = arr1.slice(1).splice([arr1[arr2, n]]);
+  console.log(arr0);
+  return arr2;
+}
+
+frankenSplice([1, 2, 3], [4, 5, 6], 1);
