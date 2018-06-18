7f573c3bb6e59528968ec968c965b7a50184b71a
Daniel <github@profoundideation.com>
2018-06-18 10:58:19 -0400

    Update Slice and Splice.js

--- 3-Javascript Algorithms And Data Structures/1-Basic JavaScript/Slice and Splice.js
+++ 3-Javascript Algorithms And Data Structures/1-Basic JavaScript/Slice and Splice.js
@@ -1,9 +1,11 @@
 function frankenSplice(arr1, arr2, n) {
   // It's alive. It's alive!
 
-  let arr0 = arr1.slice(1).splice([arr1[arr2, n]]);
-  console.log(arr0);
-  return arr2;
+  let arr0 = arr2.slice(0, arr2.length);
+  let arr3 = arr0.splice(n, 0, ...arr1);
+
+  return arr3;
+
 }
 
 frankenSplice([1, 2, 3], [4, 5, 6], 1);
