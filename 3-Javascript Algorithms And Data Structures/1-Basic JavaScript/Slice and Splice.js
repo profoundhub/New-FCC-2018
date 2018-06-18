09b630201b683b5a5f739be860e254bc41cfd00c
Daniel <github@profoundideation.com>
2018-06-18 10:58:55 -0400

    Update Slice and Splice.js

--- 3-Javascript Algorithms And Data Structures/1-Basic JavaScript/Slice and Splice.js
+++ 3-Javascript Algorithms And Data Structures/1-Basic JavaScript/Slice and Splice.js
@@ -9,3 +9,12 @@ function frankenSplice(arr1, arr2, n) {
 }
 
 frankenSplice([1, 2, 3], [4, 5, 6], 1);
+
+// running test
+/*
+  frankenSplice([1, 2, 3], [4, 5], 1) should return [4, 1, 2, 3, 5].
+  frankenSplice([1, 2], ["a", "b"], 1) should return ["a", 1, 2, "b"].
+  frankenSplice(["claw", "tentacle"], ["head", "shoulders", "knees", "toes"], 2) should return ["head", "shoulders", "claw", "tentacle", "knees", "toes"].
+  All elements from the first array should be added to the second array in their original order.
+*/
+// tests completed
