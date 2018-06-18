1256fc969363cb903437190c1293de2ebe08cf67
Daniel <github@profoundideation.com>
2018-06-18 11:02:07 -0400

    Solution
    Solution

--- 3-Javascript Algorithms And Data Structures/1-Basic JavaScript/Slice and Splice.js
+++ 3-Javascript Algorithms And Data Structures/1-Basic JavaScript/Slice and Splice.js
@@ -2,19 +2,14 @@ function frankenSplice(arr1, arr2, n) {
   // It's alive. It's alive!
 
   let arr0 = arr2.slice(0, arr2.length);
-  let arr3 = arr0.splice(n, 0, ...arr1);
-
-  return arr3;
-
+  arr0.splice(n, 0, ...arr1);
+  
+  return arr0;
 }
 
 frankenSplice([1, 2, 3], [4, 5, 6], 1);
 
 // running test
 /*
-  frankenSplice([1, 2, 3], [4, 5], 1) should return [4, 1, 2, 3, 5].
-  frankenSplice([1, 2], ["a", "b"], 1) should return ["a", 1, 2, "b"].
-  frankenSplice(["claw", "tentacle"], ["head", "shoulders", "knees", "toes"], 2) should return ["head", "shoulders", "claw", "tentacle", "knees", "toes"].
-  All elements from the first array should be added to the second array in their original order.
 */
 // tests completed
