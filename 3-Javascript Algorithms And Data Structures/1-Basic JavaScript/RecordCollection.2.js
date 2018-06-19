05d1a10cf2bcfea2df2f16be5516c5e904bf967d
Daniel Lim <hbt@markhamboards.com>
2018-06-18 22:34:36 -0400

    update ver 2 - w / es6

--- 3-Javascript Algorithms And Data Structures/1-Basic JavaScript/RecordCollection.2.js
+++ 3-Javascript Algorithms And Data Structures/1-Basic JavaScript/RecordCollection.2.js
@@ -1,27 +1,29 @@
+"use strict";
+
 // Setup
 var collection = {
     "2548": {
-      "album": "Slippery When Wet",
-      "artist": "Bon Jovi",
-      "tracks": [ 
-        "Let It Rock", 
-        "You Give Love a Bad Name" 
+        "album": "Slippery When Wet",
+        "artist": "Bon Jovi",
+        "tracks": [ 
+            "Let It Rock", 
+            "You Give Love a Bad Name" 
       ]
     },
     "2468": {
-      "album": "1999",
-      "artist": "Prince",
-      "tracks": [ 
-        "1999", 
-        "Little Red Corvette" 
-      ]
+        "album": "1999",
+        "artist": "Prince",
+        "tracks": [ 
+            "1999", 
+            "Little Red Corvette" 
+        ]
     },
     "1245": {
-      "artist": "Robert Palmer",
-      "tracks": [ ]
+        "artist": "Robert Palmer",
+        "tracks": [ ]
     },
     "5439": {
-      "album": "ABBA Gold"
+        "album": "ABBA Gold"
     }
 };
 // Keep a copy of the collection for tests
@@ -31,22 +33,20 @@ var collectionCopy = JSON.parse(JSON.stringify(collection));
 
 const updateRecords = (id, prop, value) => {
 
-  if (value !== '') {
-    if (prop === 'tracks') {
-      if (!collection[id].hasOwnProperty(prop)) {
-          collection[id][prop] = [];        
-        }
-        collection[id][prop].push(value);
-      } else {
-        collection[id][prop] = value;       
-      }         
+    if (value !== '') {
+        if (prop === 'tracks') {
+            if (!collection[id].hasOwnProperty(prop)) {
+                collection[id][prop] = [];        
+            }
+            collection[id][prop].push(value);
+        } else {
+            collection[id][prop] = value;       
+        }         
     } else {
-      delete collection[id][prop];
+        delete collection[id][prop];
     }
-
-  return collection;
+    return collection;
 }
-
 
 // Alter values below to test your code
 updateRecords(5439, "artist", "ABBA");
