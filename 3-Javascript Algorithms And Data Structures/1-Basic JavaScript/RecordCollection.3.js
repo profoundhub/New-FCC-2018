51006676f1f7b32ca287ef5b39733b3313dcbf0b
Daniel Lim <hbt@markhamboards.com>
2018-06-18 22:53:07 -0400

    stable

--- 3-Javascript Algorithms And Data Structures/1-Basic JavaScript/RecordCollection.3.js
+++ 3-Javascript Algorithms And Data Structures/1-Basic JavaScript/RecordCollection.3.js
@@ -32,15 +32,14 @@ const collectionCopy = JSON.parse(JSON.stringify(collection));
 // Only change code below this line
 
 const updateRecords = (id, prop, value) => {
-
-    if (value !== '') {
-        if (prop === 'tracks') {
+    if (value !== "") {
+        if (prop === "tracks") {
             if (!collection[id].hasOwnProperty(prop)) {
-                collection[id][prop] = [];        
+                collection[id][prop] = [];
             }
             collection[id][prop].push(value);
         } else {
-            collection[id][prop] = value;       
+            collection[id][prop] = value;
         }
     } else {
         delete collection[id][prop];
