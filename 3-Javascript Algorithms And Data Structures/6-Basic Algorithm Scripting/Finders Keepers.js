fc8171d033777435ea4b444ccd0854b7ad04e8ce
Daniel <github@profoundideation.com>
2018-06-23 12:31:52 -0400

    Update Finders Keepers.js

--- 3-Javascript Algorithms And Data Structures/6-Basic Algorithm Scripting/Finders Keepers.js
+++ 3-Javascript Algorithms And Data Structures/6-Basic Algorithm Scripting/Finders Keepers.js
@@ -1,3 +1,7 @@
 // Basic Algorithm Scripting: Finders Keepers
 
-return arr.filter(func)[0];
+function findElement(arr, func) {
+  return arr.filter(func)[0];
+}
+
+findElement([1, 2, 3, 4], num => num % 2 === 0);
