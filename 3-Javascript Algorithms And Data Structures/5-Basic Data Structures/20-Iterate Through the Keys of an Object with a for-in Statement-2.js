044e21db1dee1245525abe70a69c237521cd880f
Daniel <github@profoundideation.com>
2018-06-23 11:46:42 -0400

    Create 20-Iterate Through the Keys of an Object with a for-in Statement-2.js

---
+++ 3-Javascript Algorithms And Data Structures/5-Basic Data Structures/20-Iterate Through the Keys of an Object with a for-in Statement-2.js
@@ -0,0 +1,31 @@
+let users = {
+  Alan: {
+    age: 27,
+    online: false
+  },
+  Jeff: {
+    age: 32,
+    online: true
+  },
+  Sarah: {
+    age: 48,
+    online: false
+  },
+  Ryan: {
+    age: 19,
+    online: true
+  }
+};
+
+const countOnline = (obj) => {
+	
+  // change code below this line
+  let no = 0;
+    for (let user in obj) 
+      if (obj[user].online) no++;
+	  return no;  
+  // change code above this line
+  
+}
+
+console.log(countOnline(users));
