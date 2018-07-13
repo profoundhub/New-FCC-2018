28907e5443215885510cd6c1eb70a24809742d9b
Daniel Core <github@profoundideation.com>
2018-07-13 16:55:15 -0400

    22

--- 03-Front End Libraries/3D-React/22-Render State in the User Interface.js
+++ 03-Front End Libraries/3D-React/22-Render State in the User Interface.js
@@ -2,13 +2,13 @@ class MyComponent extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			name: 'Free Code Camp'
+			name: 'freeCodeCamp'
 		}
 	}
   render() {
     return (
 	    <div>
-			{ /* change code below this line */ }
+				{ /* change code below this line */ }
 				<h1>{this.state.name}</h1>
 	    	{ /* change code above this line */ }
 	    </div>
