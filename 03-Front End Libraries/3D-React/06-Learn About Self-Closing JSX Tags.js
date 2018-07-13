9a1aa6b3f31bf8fe0ba3e27184eabd4c00fa31f9
Daniel Core <github@profoundideation.com>
2018-07-13 13:26:09 -0400

    6 solutions

--- 03-Front End Libraries/3D-React/06-Learn About Self-Closing JSX Tags.js
+++ 03-Front End Libraries/3D-React/06-Learn About Self-Closing JSX Tags.js
@@ -3,9 +3,9 @@
 const JSX = (
     <div>
         {/* change code below this line */}
-        <h2>Welcome to React!</h2> <br >
+        <h2>Welcome to React!</h2> <br />
         <p>Be sure to close all tags!</p>
-        <hr >
+        <hr />
         {/* change code above this line */}
     </div>
 );
