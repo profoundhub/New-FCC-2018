56fa8b37e2d4d12b6a0efa7ae2640981363468e4
Daniel Core <github@profoundideation.com>
2018-07-13 13:19:01 -0400

    02

---
+++ 03-Front End Libraries/3D-React/02-Create a Complex JSX Element.js
@@ -0,0 +1,11 @@
+const JSX = (
+    <div>
+        <h1>Hello JSX!</h1>
+        <p>Some info</p>
+        <ul>
+            <li>An item</li>
+            <li>Another item</li>
+            <li>A third item</li>
+        </ul>
+    </div>
+);
