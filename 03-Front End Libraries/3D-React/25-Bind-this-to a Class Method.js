950bb788df92e5585a164e4cdc558a5679a9c747
Daniel Core <github@profoundideation.com>
2018-07-13 17:02:56 -0400

    25 Solution

--- 03-Front End Libraries/3D-React/25-Bind-this-to a Class Method.js
+++ 03-Front End Libraries/3D-React/25-Bind-this-to a Class Method.js
@@ -1,26 +1,22 @@
 class MyComponent extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        itemCount: 0
-      };
-      // change code below this line
-  
-      // change code above this line
-    }
-    addItem() {
-      this.setState({
-        itemCount: this.state.itemCount + 1
-      });
-    }
-    render() {
-      return (
-        <div>
-          { /* change code below this line */ }
-          <button>Click Me</button>
-          { /* change code above this line */ }
-          <h1>Current Item Count: {this.state.itemCount}</h1>
-        </div>
-      );
-    }
-  };
+	constructor(props) {
+		super(props);
+		this.state = {
+			itemCount: 0
+		};
+		this.addItem = this.addItem.bind(this);
+	}
+	addItem() {
+		this.setState({
+			itemCount: this.state.itemCount + 1
+		});
+	}
+	render() {
+  	return (
+	    <div>
+        <button onClick = {this.addItem}>Click Me</button>
+        <h1>Current Item Count: {this.state.itemCount}</h1>
+	    </div>
+    );
+  }
+};
