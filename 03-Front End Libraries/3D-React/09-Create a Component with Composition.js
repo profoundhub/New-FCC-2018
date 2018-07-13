4ebc3c2d602a4746f438ef379eb946f395286f7c
Daniel Core <github@profoundideation.com>
2018-07-13 14:50:58 -0400

    8-9

---
+++ 03-Front End Libraries/3D-React/09-Create a Component with Composition.js
@@ -0,0 +1,12 @@
+class MyComponent extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+  render() {
+	  // change code below this line
+
+
+
+	  // change code above this line
+  }
+};
