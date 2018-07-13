426455f802a1d501e9b260d8df0ea41a10451b4f
Daniel Core <github@profoundideation.com>
2018-07-13 16:58:27 -0400

    24 sol

--- 03-Front End Libraries/3D-React/24-Set State with this.setState.js
+++ 03-Front End Libraries/3D-React/24-Set State with this.setState.js
@@ -1,22 +1,22 @@
 class MyComponent extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        name: 'Initial State'
-      };
-      this.handleClick = this.handleClick.bind(this);
-    }
-    handleClick() {
-      // change code below this line
-  
-      // change code above this line
-    }
-    render() {
-      return (
-        <div>
-          <button onClick={this.handleClick}>Click Me</button>
-          <h1>{this.state.name}</h1>
-        </div>
-      );
-    }
-  };
+	constructor(props) {
+		super(props);
+		this.state = {
+			name: 'Initial State'
+		};
+		this.click = this.click.bind(this);
+	}
+	click() {
+		// change code below this line
+
+	  // change code above this line
+	}
+	render() {
+  	return (
+	    <div>
+        <button onClick = {this.click}>Click Me</button>
+        <h1>{this.state.name}</h1>
+	    </div>
+    );
+  }
+};
