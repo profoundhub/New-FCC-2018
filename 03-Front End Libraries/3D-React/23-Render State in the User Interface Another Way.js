cef4d35ec5712b4d7392d31d5127b94ec66db935
Daniel Core <github@profoundideation.com>
2018-07-13 16:55:24 -0400

    23

---
+++ 03-Front End Libraries/3D-React/23-Render State in the User Interface Another Way.js
@@ -0,0 +1,20 @@
+class MyComponent extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        name: 'freeCodeCamp'
+      }
+    }
+    render() {
+      // change code below this line
+  
+      // change code above this line
+      return (
+        <div>
+          { /* change code below this line */ }
+  
+          { /* change code above this line */ }
+        </div>
+      );
+    }
+  };
