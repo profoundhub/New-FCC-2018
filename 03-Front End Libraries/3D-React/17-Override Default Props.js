3c4a8fab19731ef9ebd43b4a8099407223decd2b
Daniel Core <github@profoundideation.com>
2018-07-13 15:28:21 -0400

    17 Sol

--- 03-Front End Libraries/3D-React/17-Override Default Props.js
+++ 03-Front End Libraries/3D-React/17-Override Default Props.js
@@ -1,18 +1,18 @@
 const Items = (props) => {
-    return <h1>Current Quantity of Items in Cart: {props.quantity}</h1>
+	return <h1>Current Quantity of Items in Cart: {props.quantity}</h1>
+}
+
+Items.defaultProps = {
+	quantity: 0
+}
+
+class ShoppingCart extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+  render() {
+    { /* change code below this line */ }
+    return <Items />
+    { /* change code above this line */ }
   }
-  
-  Items.defaultProps = {
-    quantity: 0
-  }
-  
-  class ShoppingCart extends React.Component {
-    constructor(props) {
-      super(props);
-    }
-    render() {
-      { /* change code below this line */ }
-      return <Items />
-      { /* change code above this line */ }
-    }
-  };
+};
