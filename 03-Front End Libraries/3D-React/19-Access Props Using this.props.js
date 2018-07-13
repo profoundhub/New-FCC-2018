f7f2df38c0180c93c6af371464e7cbc05d701d1b
Daniel Core <github@profoundideation.com>
2018-07-13 15:31:02 -0400

    19 Sol

--- 03-Front End Libraries/3D-React/19-Access Props Using this.props.js
+++ 03-Front End Libraries/3D-React/19-Access Props Using this.props.js
@@ -6,9 +6,7 @@ class ReturnTempPassword extends React.Component {
     render() {
       return (
           <div>
-                  { /* change code below this line */ }
-              <p>Your temporary password is: <strong></strong></p>
-              { /* change code above this line */ }
+              <p>Your temporary password is: <strong>{this.props.tempPassword}</strong></p>
           </div>
       );
     }
@@ -26,7 +24,7 @@ class ReturnTempPassword extends React.Component {
               <h3>We've generated a new temporary password for you.</h3>
               <h3>Please reset this password from your account settings ASAP.</h3>
               { /* change code below this line */ }
-  
+            <ReturnTempPassword tempPassword="serrPbqrPnzc" />
               { /* change code above this line */ }
           </div>
       );
