139482fb1dee6df15324b5f9cccf37c7940a967e
Daniel Core <github@profoundideation.com>
2018-07-13 15:31:32 -0400

    20

---
+++ 03-Front End Libraries/3D-React/20-Review Using Props with Stateless Functional Components.js
@@ -0,0 +1,14 @@
+class CampSite extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      return (
+        <div>
+          <Camper/>
+        </div>
+      );
+    }
+  };
+  // change code below this line
+  
