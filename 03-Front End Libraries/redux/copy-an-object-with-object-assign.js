65e60e923aeaf9a3bafebdac3e23628bb9796e78
Daniel <github@profoundideation.com>
2018-07-14 12:15:36 -0400

    Update copy-an-object-with-object-assign.js

--- 03-Front End Libraries/redux/copy-an-object-with-object-assign.js
+++ 03-Front End Libraries/redux/copy-an-object-with-object-assign.js
@@ -1,1 +1,25 @@
 
+const defaultState = {
+  user: 'CamperBot',
+  status: 'offline',
+  friends: '732,982',
+  community: 'freeCodeCamp'
+};
+
+const immutableReducer = (state = defaultState, action) => {
+  switch(action.type) {
+    case 'ONLINE':
+      // don't mutate state here or the tests will fail
+      return
+    default:
+      return state;
+  }
+};
+
+const wakeUp = () => {
+  return {
+    type: 'ONLINE'
+  }
+};
+
+const store = Redux.createStore(immutableReducer);
