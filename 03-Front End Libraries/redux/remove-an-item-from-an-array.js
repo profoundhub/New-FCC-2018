9fcd7799103bcc58b0218ef111d577277d78d443
Daniel <github@profoundideation.com>
2018-07-14 11:57:41 -0400

    Update remove-an-item-from-an-array.js

--- 03-Front End Libraries/redux/remove-an-item-from-an-array.js
+++ 03-Front End Libraries/redux/remove-an-item-from-an-array.js
@@ -1,1 +1,19 @@
 
+const immutableReducer = (state = [0,1,2,3,4,5], action) => {
+  switch(action.type) {
+    case 'REMOVE_ITEM':
+      // don't mutate state here or the tests will fail
+      return
+    default:
+      return state;
+  }
+};
+
+const removeItem = (index) => {
+  return {
+    type: 'REMOVE_ITEM',
+    index
+  }
+}
+
+const store = Redux.createStore(immutableReducer);
