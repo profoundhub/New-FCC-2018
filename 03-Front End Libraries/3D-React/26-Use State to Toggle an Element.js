1924a6a0098158cbdc5dc26d047acf811abe5c4d
Daniel Core <github@profoundideation.com>
2018-07-15 22:35:35 -0400

    fixed merge error

--- 03-Front End Libraries/3D-React/26-Use State to Toggle an Element.js
+++ 03-Front End Libraries/3D-React/26-Use State to Toggle an Element.js
@@ -1,34 +1,35 @@
+
 class MyComponent extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        visibility: false
-      };
-      // change code below this line
-  
-      // change code above this line
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      visibility: false
+    };
     // change code below this line
-    toggleVisibility = () => {
-          this.setState({
-              visibility: !this.state.visibility
-          });
-      }
+    this.toggleVisibility.bind(this);
     // change code above this line
-    render() {
-      if (this.state.visibility) {
-        return (
-          <div>
-            <button onClick={this.toggleVisibility}>Click Me</button>
-            <h1>Now you see me!</h1>
-          </div>
-        );
-      } else {
-        return (
-          <div>
-            <button onClick={this.toggleVisibility}>Click Me</button>
-          </div>
-        );
-      }
+  }
+  // change code below this line
+    toggleVisibility = () => {
+		  this.setState({
+			  visibility: !this.state.visibility
+		  });
+	  }
+  // change code above this line
+  render() {
+    if (this.state.visibility) {
+      return (
+        <div>
+          <button onClick={this.toggleVisibility}>Click Me</button>
+          <h1>Now you see me!</h1>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <button onClick={this.toggleVisibility}>Click Me</button>
+        </div>
+      );
     }
-  };
+  }
+};
