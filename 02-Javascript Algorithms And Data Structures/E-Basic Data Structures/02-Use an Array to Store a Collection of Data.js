c773fa7e83cd998471ecc70603d9bbc830eda222
Daniel <github@profoundideation.com>
2018-07-16 11:17:57 -0400

    Update 02-Use an Array to Store a Collection of Data.js

--- 02-Javascript Algorithms And Data Structures/E-Basic Data Structures/02-Use an Array to Store a Collection of Data.js
+++ 02-Javascript Algorithms And Data Structures/E-Basic Data Structures/02-Use an Array to Store a Collection of Data.js
@@ -1,3 +1,4 @@
 // Use an Array to Store a Collection of Data
 
-let yourArray; // change this line
+let yourArray = ['one', 2, 'three', true, false, undefined, null];
+console.log(yourArray.length);
