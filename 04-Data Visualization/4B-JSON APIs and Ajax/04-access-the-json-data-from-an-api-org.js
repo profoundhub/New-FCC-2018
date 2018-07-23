0c4ccac00b283374c4f0402a371a37c745aee31b
Daniel Core <github@profoundideation.com>
2018-07-23 11:54:14 -0400

    reset- ++

--- 04-Data Visualization/4B-JSON APIs and Ajax/04-access-the-json-data-from-an-api-org.js
+++ 04-Data Visualization/4B-JSON APIs and Ajax/04-access-the-json-data-from-an-api-org.js
@@ -1,16 +1,16 @@
 <script>
   document.addEventListener('DOMContentLoaded',function(){
     document.getElementById('getMessage').onclick=function(){
-      // Add your code below this line
       req=new XMLHttpRequest();
       req.open("GET",'/json/cats.json',true);
       req.send();
       req.onload=function(){
         json=JSON.parse(req.responseText);
         document.getElementsByClassName('message')[0].innerHTML=JSON.stringify(json);
+        // Add your code below this line
+        
+        // Add your code above this line
       };
-      
-      // Add your code above this line
     };
   });
 </script>
--- 04-Data Visualization/4B-JSON APIs and Ajax/04-access-the-json-data-from-an-api-sol.js
+++ 04-Data Visualization/4B-JSON APIs and Ajax/04-access-the-json-data-from-an-api-sol.js
@@ -1,16 +1,19 @@
 <script>
   document.addEventListener('DOMContentLoaded',function(){
     document.getElementById('getMessage').onclick=function(){
-      // Add your code below this line
       req=new XMLHttpRequest();
       req.open("GET",'/json/cats.json',true);
       req.send();
       req.onload=function(){
         json=JSON.parse(req.responseText);
         document.getElementsByClassName('message')[0].innerHTML=JSON.stringify(json);
+        // Add your code below this line
+        
+        console.log(json[0].altText);
+        // Prints "A white cat wearing a green helmet shaped melon on its head."
+
+        // Add your code above this line
       };
-      
-      // Add your code above this line
     };
   });
 </script>
