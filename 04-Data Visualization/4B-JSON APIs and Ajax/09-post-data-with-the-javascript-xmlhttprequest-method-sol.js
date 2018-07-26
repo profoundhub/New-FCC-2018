12888b6620ddd7d7f5424088d71cc5a1dc66426b
Daniel Core <github@profoundideation.com>
2018-07-26 12:49:04 -0400

    UPDATE SOL

--- 04-Data Visualization/4B-JSON APIs and Ajax/09-post-data-with-the-javascript-xmlhttprequest-method-sol.js
+++ 04-Data Visualization/4B-JSON APIs and Ajax/09-post-data-with-the-javascript-xmlhttprequest-method-sol.js
@@ -4,7 +4,15 @@
     
       var userName=document.getElementById('name').value;
       // Add your code below this line
-      
+      req=new XMLHttpRequest();
+      req.open("POST",url,true);
+      req.setRequestHeader('Content-Type','text/plain');
+      req.onreadystatechange=function(){
+        if(req.readyState==4 && req.status==200){
+          document.getElementsByClassName('message')[0].innerHTML=req.responseText;
+        }
+      };
+      req.send(userName);
       
       // Add your code above this line
     };
